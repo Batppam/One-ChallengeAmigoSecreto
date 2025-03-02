bf58226dc2d2a823fa54cd69e0a971c8189149da
Batpam <peronpamela@gmail.com>
2025-03-02 13:58:12 -0300

    projeto Inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+let participantes = ['Joao','Carla','Adond', 'poli']; //criar array com nome dos participantes
+
+
+function gerarAmigoAleatorio(){
+    let amigoSorteado; //funçao rondomica
+
+    if (quantidadeDeParticipantes == participantes.length){ //Avisar quando não tiver mais nomes na lista
+        alert('Todos foram sorteados');
+    }
+    if (participantes.includes(amigoSorteado)){
+        return gerarAmigoAleatorio;
+    } else {
+        participantes.push(amigoSorteado);
+        console.log(participantes);
+        return amigoSecreto;
+    }
+
+}
